dbcc5509d3361363342682bdac36425a63eb6fba
meltingice <meltingice8917@gmail.com>
2011-09-07 16:06:09 -0400

    Some simple style/syntax changes

--- plugins/threshold.js
+++ plugins/threshold.js
@@ -1,30 +1,25 @@
-if (!Caman && typeof exports == "object") {
+/*global Caman: true, exports: true */
 
+/*
+ * NodeJS compatibility
+ */
+if (!Caman && typeof exports == "object") {
   var Caman = {manip:{}};
   exports.plugins = Caman.manip;
 }
 
 (function (Caman) {
 
-
-
 Caman.manip.threshold = function(adjust) {
-
-  console.log("Thresholding at "+adjust);
+  return this.process( adjust, function threshold (adjust, rgba) {
   
-  return this.process( adjust, function thresholding(adjust, rgba) {
-  
-    var luminance = (0.2126*rgba.r) + (0.7152*rgba.g) + (0.0722*rgba.b)
+    var luminance = (0.2126*rgba.r) + (0.7152*rgba.g) + (0.0722*rgba.b);
     
-    if (luminance < adjust)
-    {
+    if (luminance < adjust) {
       rgba.r = 0;
       rgba.g = 0;
       rgba.b = 0;
-    }
-
-    else
-    {
+    } else {
       rgba.r = 255;
       rgba.g = 255;
       rgba.b = 255;
