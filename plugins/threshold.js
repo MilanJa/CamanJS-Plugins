cf8fc2127a085210210111992c4b63a4ae47d7e3
Milan Jansen <milanjansen@gmail.com>
2011-09-07 21:50:34 +0200

    Thresholding plugin

---
+++ plugins/threshold.js
@@ -0,0 +1,37 @@
+if (!Caman && typeof exports == "object") {
+
+  var Caman = {manip:{}};
+  exports.plugins = Caman.manip;
+}
+
+(function (Caman) {
+
+
+
+Caman.manip.threshold = function(adjust) {
+
+  console.log("Thresholding at "+adjust);
+  
+  return this.process( adjust, function thresholding(adjust, rgba) {
+  
+    var luminance = (0.2126*rgba.r) + (0.7152*rgba.g) + (0.0722*rgba.b)
+    
+    if (luminance < adjust)
+    {
+      rgba.r = 0;
+      rgba.g = 0;
+      rgba.b = 0;
+    }
+
+    else
+    {
+      rgba.r = 255;
+      rgba.g = 255;
+      rgba.b = 255;
+    }
+
+    return rgba;
+  });
+};
+
+}(Caman));
